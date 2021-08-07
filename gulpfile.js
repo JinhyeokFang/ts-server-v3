46e670ece43721e1c2bd12e7847d464aa0516651
jinhyeokfang <jinhyeokfang@gmail.com>
2021-08-07 16:08:47 +0900

    FIX

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,36 @@
-const gulp = require("gulp");
-const ts = require("gulp-typescript");
-const eslint = require("gulp-eslint");
-const uglify = require("gulp-uglify");
-const tsProject = ts.createProject("tsconfig.json");
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const eslint = require('gulp-eslint');
+const uglify = require('gulp-uglify');
+const watch = require('gulp-watch');
+const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task("build", function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js
-        .pipe(uglify())
-        .pipe(gulp.dest("dist"));
+gulp.task('build', () => {
+  return tsProject.src()
+    .pipe(tsProject())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task("devBuild", function () {
-    return tsProject.src()
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError())
-        .pipe(tsProject())
-        .js
-        .pipe(gulp.dest("dist"));
-})
+gulp.task('devBuild', () => {
+  return tsProject.src()
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(tsProject())
+    .pipe(gulp.dest('dist'));
+});
 
-gulp.task("lint", function () {
-    return tsProject.src()
-        .pipe(eslint({fix: true}))
-        .pipe(eslint.format())
-        .pipe(gulp.dest(file => file.base))
-        .pipe(eslint.failAfterError())
-})
+gulp.task('lint', () => {
+  return tsProject.src()
+    .pipe(eslint({fix: true}))
+    .pipe(eslint.format())
+    .pipe(gulp.dest(file => file.base))
+    .pipe(eslint.failAfterError());
+});
 
-gulp.task("default", gulp.series("build"));
+gulp.task('watch', () => {
+  watch('./**/*.ts', gulp.series('devBuild'));
+});
+
+gulp.task('default', gulp.series('build'));
