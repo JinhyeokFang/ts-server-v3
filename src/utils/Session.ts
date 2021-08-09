ece20781f2d321d75e6bdbd75eefd44e2d972f1d
jinhyeokfang <jinhyeokfang@gmail.com>
2021-08-09 14:47:41 +0900

    FIX session util

--- src/utils/Session.ts
+++ src/utils/Session.ts
@@ -5,15 +5,22 @@ import { Application } from 'express';
 
 const RedisStore = connectRedis(session);
 
+// 반드시 세션으로 로그인 구현시 유저 정보에 ip를 함께 저장할 것.
 export default function initialize(app: Application,
   port: number, host: string, secret: string): void {
   app.use(session({
     secret,
+    httpOnly: true,
+    secure: true,
     store: new RedisStore({
       client: redis.createClient(port, host),
       ttl: 200,
     }),
     saveUninitialized: false,
     resave: false,
+    cookie: {
+      httpOnly: true,
+      secure: true,
+    },
   }));
 }
