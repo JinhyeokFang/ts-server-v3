73922c40e35cc40c762da386f91cdaeab188c5a1
jinhyeokfang <jinhyeokfang@gmail.com>
2021-08-13 21:47:34 +0900

    .

--- src/modules/logger.ts
+++ src/modules/logger.ts
@@ -1,25 +1,25 @@
-import winston from 'winston';
-import WinstonDaily from 'winston-daily-rotate-file';
+// import winston from 'winston';
+// import WinstonDaily from 'winston-daily-rotate-file';
 
-export default winston.createLogger({
-  transports: [
-    new WinstonDaily({
-      level: 'info',
-      datePattern: 'YYYY-MM-DD',
-      dirname: 'logs/info',
-      filename: '%DATE%.log',
-      maxSize: '20m',
-      maxFiles: '14d',
-      zippedArchive: true,
-    }),
-    new WinstonDaily({
-      level: 'error',
-      datePattern: 'YYYY-MM-DD',
-      dirname: 'logs/error',
-      filename: '%DATE%.log',
-      maxSize: '20m',
-      maxFiles: '14d',
-      zippedArchive: true,
-    }),
-  ],
-});
+// export default winston.createLogger({
+//   transports: [
+//     new WinstonDaily({
+//       level: 'info',
+//       datePattern: 'YYYY-MM-DD',
+//       dirname: 'logs/info',
+//       filename: '%DATE%.log',
+//       maxSize: '20m',
+//       maxFiles: '14d',
+//       zippedArchive: true,
+//     }),
+//     new WinstonDaily({
+//       level: 'error',
+//       datePattern: 'YYYY-MM-DD',
+//       dirname: 'logs/error',
+//       filename: '%DATE%.log',
+//       maxSize: '20m',
+//       maxFiles: '14d',
+//       zippedArchive: true,
+//     }),
+//   ],
+// });
